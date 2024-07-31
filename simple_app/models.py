22a74b4e54b8affc6c719dfbaa13c283c97f93b0
kekW <panicua12@gmail.com>
2024-07-31 21:36:15 +0300

    add: model of Book(title,author,price)

--- simple_app/models.py
+++ simple_app/models.py
@@ -1,3 +1,22 @@
+from decimal import Decimal
+
+from django.core.validators import MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    price = models.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        validators=[MinValueValidator(Decimal("0.00"))]
+    )
+
+    class Meta:
+        ordering = ["title"]
+        verbose_name = "Book"
+        verbose_name_plural = "Books"
+
+    def __str__(self):
+        return f"{self.title} by {self.author}"
