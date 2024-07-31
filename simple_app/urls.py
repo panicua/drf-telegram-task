e89ac8ace3c4d8811e6b640904061be753a3662b
kekW <panicua12@gmail.com>
2024-07-31 22:06:22 +0300

    init,add: drf. Created route, view and serializer for books endpoint

--- simple_app/urls.py
+++ simple_app/urls.py
@@ -1,10 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
 
 from . import views
 
 
 app_name = "simple_app"
 
+router = routers.DefaultRouter()
+router.register("", views.BookViewSet, basename="books")
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path("api/books/", include(router.urls)),
 ]
--- simple_app/views.py
+++ simple_app/views.py
@@ -1,7 +1,15 @@
-from django.shortcuts import render
-
 from django.http import HttpResponse
+from rest_framework import viewsets
+from rest_framework.mixins import ListModelMixin
+
+from simple_app.models import Book
+from simple_app.serializers import BookSerializer
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
+
+
+class BookViewSet(ListModelMixin, viewsets.GenericViewSet):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
