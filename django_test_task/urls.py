21c4ff649202577f0e89a46647ef51224d8f6854
kekW <panicua12@gmail.com>
2024-07-31 21:45:54 +0300

    init,add: urls.py in simple_app with route to index page

--- django_test_task/urls.py
+++ django_test_task/urls.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path(
+        "",
+        include("simple_app.urls", namespace="simple_app"),
+    ),
 ]
