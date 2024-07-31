e0945236ad10914c4934934e59b7462c07186b26
kekW <panicua12@gmail.com>
2024-08-01 01:18:05 +0300

    ref: router.register(''>'books'), path('api/books'>'api/')

--- simple_app/urls.py
+++ simple_app/urls.py
@@ -1,15 +1,15 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from . import views
+from simple_app import views
 
 
 app_name = "simple_app"
 
 router = routers.DefaultRouter()
-router.register("", views.BookViewSet, basename="books")
+router.register("books", views.BookViewSet, basename="books")
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("api/books/", include(router.urls)),
+    path("api/", include(router.urls)),
 ]
